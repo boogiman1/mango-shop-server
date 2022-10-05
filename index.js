36504c2be13bcf31b88f6d88c7d0da388f2387b8
boogiman1 <boowon1@naver.com>
2022-10-05 10:20:25 +0900

    서버 라우팅 적용

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-var http = require("http");
-var hostname='127.0.0.1';
-var port='8080';
+var express = require("express");
+var cors = require("cors");
+var app = express();
+var port = 8080;
 
-const server=http.createServer(function(req,res){
-    const path=req.url;
-    const method=req.method;
-    if(path === '/products'){
-        if(method === 'GET'){
-            res.writeHead(200,{"Content-Type" :"application/json"});
-            const products = JSON.stringify([{name:"배변패드", price :50000}])
-            res.end(products);
-        }else if(method ==='POST'){
-            res.end("❤ POST success");
-        }
-    res.end('GOOD BOY')
+app.use(express.json());
+app.use(cors());
+app.get('/products', (req, res) => {
+    const queryString=req.query;
+    console.log(req,queryString);
+    res.send({
+        "products": [
+            { "id": 1, "name": "습식사료", "price": 10000, "seller": "내추럴코어", "imageUrl": "images/products/food1.jpg" },
+            { "id": 2, "name": "하네스", "price": 50000, "seller": "도기멍", "imageUrl": "images/products/acc1.jpg" },
+            { "id": 3, "name": "배변패드", "price": 30000, "seller": "흡수혁명", "imageUrl": "images/products/house1.jpg" }
+        ]
     }
+    )
+
+})
+app.get('/products/:id/events/:eventId', (req, res) => {
+    const params=req.params;
+    const {id,eventId}=params;
+    res.send(`id는 ${id}와 ${eventId} 입니다`);
+
 })
 
-server.listen(port, hostname)
-console.log('mangoshop server on');
+app.post('/products', (req, res) => {
+    const body=req.body;
+    res.send({
+        body:body
+    })
+})
+
+app.post('/login', (req, res) => {
+    res.send('로그인이 완료 되었습니다.')
+})
+
+app.listen(port, () => {
+    console.log('망고샵의 서버가 돌아가고 있습니다.');
+})
